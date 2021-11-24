06ab922c460cb531eb7960915c35ff9c42be0a64
jaden <jadenbusiness@hotmail.com>
2021-11-24 22:40:10 +0000

    last implement

---
+++ without.js
@@ -0,0 +1,34 @@
+const eqArrays = function (arr1, arr2) {
+  let ans;
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        ans = true;
+      } else {
+        ans = false;
+      }
+    }
+  }
+  return ans;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("Assertion Passed");
+  } else {
+    console.log("Assertion Failed");
+  }
+};
+
+const without = function (source, remove) {
+  let newArr = source.filter(item => !remove.includes(item))
+  return newArr;
+}
+
+
+console.log(without([1, 2, 3], [2]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
