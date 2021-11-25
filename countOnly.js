c348aac56f0538c804f73cefb9c64b630c52ccc5
jaden <jadenbusiness@hotmail.com>
2021-11-25 19:56:30 +0000

    countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const countOnly = function (allItems, itemsToCount) {
+  const obj = {};
+  for (const names of allItems) {
+    if (itemsToCount[names]) {
+      if (obj.hasOwnProperty(names)) {
+        obj[names] = obj[names] + 1
+      } else {
+        obj[names] = 1
+      }
+    }
+  }
+  return obj;
+};
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
