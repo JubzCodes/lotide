13f6752e0e41757a8049e91548996df31d405076
jaden <jadenbusiness@hotmail.com>
2021-11-25 22:48:02 +0000

    findByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+const findKeyByValue = function (bestTVShowsByGenre, genre) {
+  const keys = Object.keys(bestTVShowsByGenre);
+
+  for (const values of keys) {
+    if (bestTVShowsByGenre[values] === genre) {
+      return values;
+    }
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
