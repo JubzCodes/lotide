02f65689d46aaaf1d49afd109ca82e5e31967980
jaden <jadenbusiness@hotmail.com>
2021-11-27 20:54:09 +0000

    implement challenges

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+findKey = function (obj, callback) {
+  for (const values in obj){
+    if(callback(obj[values])){
+    return values
+    }
+  }
+}
+
+const results = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3)
+
+console.log(results);
+
+assertEqual(results,"Akaleri")
---
+++ takeUntil.js
@@ -0,0 +1,40 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const data = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+takeUntil = function(array, callback) {
+  
+  let results = [];
+  for (const values of array) {
+    if (callback(values)) {
+      results.push(values);
+    }
+  }
+  return results;
+}
+
+
+const results1 = takeUntil(data, x => x > 0);
+console.log(results1);
+assertArraysEqual(results1,[
+  1, 2, 5, 7,
+  2, 2, 4, 5
+])
