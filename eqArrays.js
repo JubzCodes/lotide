aa819b66ea7cb64db00a09f5fa432b96a0f540cd
jaden <jadenbusiness@hotmail.com>
2021-11-30 20:17:46 +0000

    refactor asserArraysEqual

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,3 @@
-const assertEqual = require('./assertEqual');
 
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
